fb8661ea52ed95785cd8763e13b030f4d9f08c7a
Bob_chemist <bob_chemist@mail.ru>
2019-04-13 00:15:37 +0300

    payload for random values added

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { createStore } from 'redux';
+
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state + action.payload;
+    default:
+      return 0;
+  }
+};
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({ type: 'INC' });
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({ type: 'DEC' });
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const payload = Math.floor(Math.random() * 10);
+  store.dispatch({ type: 'RND', payload });
+});
+
+const update = () => {
+  document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(update);
